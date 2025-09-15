2c814d27725f52a3f638b0d5480e11fbc09110b7
Josh McArthur <joshua.mcarthur@gmail.com>
2025-09-16 08:41:45 +1200

    Update schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_13_215034) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_15_065104) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -74,6 +74,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_13_215034) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug", null: false
+    t.index ["slug"], name: "index_jurisdictions_on_slug", unique: true
   end
 
   create_table "messages", force: :cascade do |t|
