1d59d19b8184f0c68ea455d9eaa75ce39d332eff
Josh McArthur <joshua.mcarthur@gmail.com>
2025-09-17 20:34:41 +1200

    Bug: Remove incompatible config from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,13 +18,6 @@ Rails.application.configure do
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
-  # Configure HTTP caching headers for better Cloudflare integration
-  config.action_controller.default_static_headers = {
-    "Cache-Control" => "public, max-age=3600",
-    "Vary" => "Accept-Encoding"
-  }
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
