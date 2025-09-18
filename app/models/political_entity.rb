be1e355e592d15c7cdd92a41864732a2cb65228e
Josh McArthur <joshua.mcarthur@gmail.com>
2025-09-18 13:38:26 +1200

    Feature: Use more efficient profile images

--- app/models/political_entity.rb
+++ app/models/political_entity.rb
@@ -3,7 +3,10 @@ class PoliticalEntity < ApplicationRecord
   has_many :political_entity_jurisdictions
   has_many :jurisdictions, through: :political_entity_jurisdictions
   has_many :interests, through: :political_entity_jurisdictions
-  has_one_attached :profile_image
+  has_one_attached :profile_image do |attachable|
+    attachable.variant :thumbnail, resize_to_limit: [ 256, 256 ], format: :webp
+    attachable.variant :small, resize_to_limit: [ 128, 128 ], format: :webp
+  end
 
   def to_param
     [ id, name.parameterize ].join("-")
