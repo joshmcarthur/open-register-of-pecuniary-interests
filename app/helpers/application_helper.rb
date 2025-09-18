c161ef19aa0e1491cc439d450f71c00741a2a5ec
Josh McArthur <joshua.mcarthur@gmail.com>
2025-09-18 15:36:15 +1200

    Feature: Use party logos instead of text badges

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,21 @@ module ApplicationHelper
       "badge-neutral"
     end
   end
+
+  def political_party_badge(party, **kwargs)
+    case party
+    when "National"
+      image_tag("party-logos/nz-national.png", **kwargs.reverse_merge(alt: "National Party"))
+    when "Labour"
+      image_tag("party-logos/nz-labour.svg", **kwargs.reverse_merge(alt: "Labour Party"))
+    when "Green"
+      image_tag("party-logos/nz-green.svg", **kwargs.reverse_merge(alt: "Green Party"))
+    when "NZ First"
+      image_tag("party-logos/nz-new-zealand-first.png", **kwargs.reverse_merge(alt: "NZ First Party"))
+    when "ACT"
+      image_tag("party-logos/nz-act.svg", **kwargs.reverse_merge(alt: "ACT Party"))
+    when "Te Pāti Māori"
+      image_tag("party-logos/nz-te-pati-maori.svg", **kwargs.reverse_merge(alt: "Te Pāti Māori"))
+    end
+  end
 end
